ee19f8a6bfa5a43066cc74cbb40ea3787cf23829
PedroCassunde <>
2025-03-20 14:57:05 -0400

    .

--- routes/veiculoRoutes.js
+++ routes/veiculoRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const veiculoController = require("../controllers/veiculoController");
+const veiculoController = require('../controllers/veiculoControllers');
 
 router.get("/cadastro", veiculoController.formCadastro);
 
