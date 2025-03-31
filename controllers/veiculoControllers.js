48187eb66097146530221d2f70433ddc2a4b862b
Hner1qwe <henriquetrachtafilho@gmail.com>
2025-03-31 17:33:40 -0400

    conexão com o prisma

--- controllers/veiculoControllers.js
+++ controllers/veiculoControllers.js
@@ -1,4 +1,8 @@
+
 const path = require("path");
+
+const { PrismaClient } = require("@prisma/client");
+const client = new PrismaClient();
 
 class veiculoController {
 
@@ -6,8 +10,19 @@ class veiculoController {
         res.sendfile(path.join(__dirname, "../", "views", "formVeiculo.html"));
     }
 
-    static cadastrar(req, res) {
-        res.send(JSON.stringify(req.body));
+    static async cadastrar(req, res) {
+
+        const {modelo, placa, ano, cor} = req.body;
+
+       const veiculo = await client.veiculo.create({data:{
+            modelo,
+            placa,
+            ano,
+            cor
+        }});
+         
+        res.send(veiculo.id);
+
     }
 
     static buscarTodos(req, res) {
